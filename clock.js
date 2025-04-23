7409ca51aa8a7694a3ddd84400123a309b0fc378
Agash2002 <115020297+Agash2002@users.noreply.github.com>
2025-04-23 12:49:42 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,12 +1,1 @@
-  let time = document.getElementById('time-el');
-  let date = document.getElementById('date-el');
 
-  function timer() {
-    let currTime = new Date().toLocaleTimeString();
-    let currDate = new Date().toLocaleDateString();
-
-    time.textContent = currTime;
-    date.textContent = currDate;
-  }
-
-  setInterval(timer, 1000);
